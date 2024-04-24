df83c381629e543ea168cff214ad4b370ceb4bb8
Jacob Adili <63510880+MisterAdili@users.noreply.github.com>
2024-04-23 19:47:17 -0500

    Update application.js

---
+++ js/application.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('.item-subtotal').change(function(){
+    $(this).text = $(this).parent().children('.item-price') * $(this).parent().children('.item-qty');
+  }
+}

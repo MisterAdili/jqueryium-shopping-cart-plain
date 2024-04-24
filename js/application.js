e2cc847636e70da37017ce8bbbcc05898848366c
Jacob Adili <63510880+MisterAdili@users.noreply.github.com>
2024-04-23 21:20:04 -0500

    Update application.js

--- js/application.js
+++ js/application.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   
   var sum = function (){
     var items = $(".item");
-    for (i = 0; i<item.length; i++){
+    for (i = 0; i<items.length; i++){
       var subtotal = '5';
       $($('.item-subtotal')[i]).text(subtotal); 
     }
